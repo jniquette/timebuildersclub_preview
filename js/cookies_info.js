4f8693c89ea84f20b78dcef13c1a4ec898d19763
Justin <justin@niquette.us>
2022-09-20 16:15:59 -0700

    moved cookies text to /js/cookies_info.js

--- js/cookies_info.js
+++ js/cookies_info.js
@@ -8,6 +8,17 @@ function hideCookiesTooltip() {
 	document.getElementById("cookiesTooltip").classList.remove("visible")
 }
 
+/* Append cookies divs to end of <body> tag */
+document.getElementsByTagName("body")[0].innerHTML += "\n" +
+	"<div id=\"cookiesButton\">\n" +
+	"\tCookies Info\n" +
+	"</div>\n" +
+	"<div id=\"cookiesTooltip\">\n" +
+	"\t<p>This site uses cookies for marketing, performance monitoring, and to ensure that you get the best experience on our site.\n" +
+	"\tIf you do not wish for cookies to be used, please disable cookies in your web browser.\n" +
+	"\t\t<span id=\"closeTooltip\">[close]</span></p>\n" +
+	"</div>";
+
 document.getElementById("cookiesButton").addEventListener("mouseover", showCookiesTooltip);
 document.getElementById("cookiesButton").addEventListener("click", showCookiesTooltip);
 document.getElementById("cookiesButton").addEventListener("mouseout", hideCookiesTooltip);
